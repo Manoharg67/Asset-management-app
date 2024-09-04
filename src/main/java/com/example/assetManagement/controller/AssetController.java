f5d5ae515ee5c3a80adbe9bcd3fcdb2abedffffc
Manohar Guradasu <manohar.guradasu@bluepal.com>
2024-09-04 12:25:58 +0530

    new fixes

--- src/main/java/com/example/assetManagement/controller/AssetController.java
+++ src/main/java/com/example/assetManagement/controller/AssetController.java
@@ -7,6 +7,7 @@ import com.example.assetManagement.model.Asset;
 import com.example.assetManagement.service.AssetService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
@@ -20,7 +21,7 @@ public class AssetController {
     private AssetService assetService;
 
     @GetMapping
-    public List<Asset> getAllAssets(@RequestParam(defaultValue = "0") int page,
+    public Page<Asset> getAllAssets(@RequestParam(defaultValue = "0") int page,
                                     @RequestParam(defaultValue = "100") int size) {
         Pageable pageable = PageRequest.of(page, size);
         return assetService.getAllAssets(pageable);
--- src/main/java/com/example/assetManagement/service/AssetService.java
+++ src/main/java/com/example/assetManagement/service/AssetService.java
@@ -1,9 +1,9 @@
 package com.example.assetManagement.service;
 
-import java.util.List;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
@@ -17,8 +17,8 @@ public class AssetService {
 	@Autowired
     private AssetRepository assetRepository;
 
-    public List<Asset> getAllAssets(Pageable pageable) {
-        return assetRepository.findAll(pageable).getContent();
+    public Page<Asset> getAllAssets(Pageable pageable) {
+        return assetRepository.findAll(pageable);
     }
 
     public Asset getAssetById(UUID id) {
